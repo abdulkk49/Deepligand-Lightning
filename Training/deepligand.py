1fe0117824f96cc0c92c123403990dcfc12ce561
AbdulKadir Khimani <48808790+abdulkk49@users.noreply.github.com>
2020-07-04 14:05:13 +0530

    Update deepligand.py

--- Training/deepligand.py
+++ Training/deepligand.py
@@ -256,8 +256,8 @@ if __name__ == "__main__"
 
     CVsplit(baseAllData, trainDir, valDir)
     system(' '.join(['cp', join(dirname(abspath("__file__")), 'data.py'), join(bilm.__path__[0])]))
-    system(' '.join(['python {}/datasets/preprocess.py -f {}/trial -o {}/train/trainraw -a {}'.format(pwd, pwd, pwd, "train")]))
-    system(' '.join(['python {}/datasets/preprocess.py -f {}/trial -o {}/val/valraw -a {}'.format(pwd, pwd, pwd, "val")]))
+    system(' '.join(['python {}/datasets/preprocess.py -o {}/trial -f {}/train/trainraw -a {}'.format(pwd, pwd, pwd, "train")]))
+    system(' '.join(['python {}/datasets/preprocess.py -o {}/trial -f {}/val/valraw -a {}'.format(pwd, pwd, pwd, "val")]))
 
     model_arch = 'mhccat2pep_pepres_relation_massspec_elmo_novar_v3_normal_noeps_bs1024_init1'
     outdir = join(join(pwd, "data"), model_arch)
